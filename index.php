6c37a48c91a28ad8d8bb326154fbd23b6ae5ef77
leon baraza <leonbaraza@gmail.com>
2019-06-23 21:46:00 +0300

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,118 @@
+<?php include('header.php');?>
+
+
+<html>
+<head>
+</head>
+<body>
+<div class="l-titlebar parallax_fixed size_large color_default">
+<div class="l-titlebar-overlay" style="background-color:rgba(0,0,0,0.05)">
+</div>
+<div class="l-titlebar-h">
+<div class="l-titlebar-content"><h1 itemprop="headline">Welcome to Leon's Assignment showing diffrent UNEP Programs</h1></div>
+</div>
+</div>
+
+
+
+
+
+<div class="l-main">
+	<div class="l-main-h i-cf">
+
+		<main class="l-contents" itemprop="mainContentOfPage">
+
+		
+		<section class="l-section wpb_row height_medium">
+<div class="l-sections-h i-cf">
+<div class="g-cols vc_row type_default valign_top">
+<div class="vc_col-sm-12 wpb_column vc_column_container">
+<div class="vc_column-inner">
+
+
+		
+
+
+
+		<section class="l-section wpb_row height_medium"><div class="l-section-h i-cf"><div class="g-cols vc_row type_default valign_top"><div class="vc_col-sm-12 wpb_column vc_column_container"><div class="vc_column-inner"><div class="wpb_wrapper"><div class="g-cols wpb_row type_default valign_top vc_inner "><div class="vc_col-sm-12 wpb_column vc_column_container"><div class="vc_column-inner"><div class="wpb_wrapper">
+	<div class="wpb_text_column ">
+		<div class="wpb_wrapper">
+			<h2 style="text-align: center"><strong><span id="internal-source-marker_0.8555400993209332">What the site does</span></strong></h2>
+
+		</div>
+	</div>
+<div class="w-separator type_invisible size_medium thick_1 style_solid color_border cont_none"><span class="w-separator-h"></span></div></div></div></div></div><div class="w-tabs layout_default icon_chevron iconpos_right accordion"><div class="w-tabs-list items_4"><div class="w-tabs-list-h">
+
+
+<div class="w-tabs-item active"><a href="#1505191960284-ce489d35-08fa" class="w-tabs-item-h"><span class="w-tabs-item-title">Principles</span></a><span class="ripple-container"></span></div>
+
+</div></div>
+
+<div class="w-tabs-sections active">
+<div class="w-tabs-sections-h" style="">
+
+<div class="w-tabs-section" id="1505191960284-ce489d35-08fa"><a href="#1505191960284-ce489d35-08fa" class="w-tabs-section-header"><div class="w-tabs-section-header-h"><h5 class="w-tabs-section-title">Principles</h5><div class="w-tabs-section-control"></div></div></a><div class="w-tabs-section-content" style="display: block;"><div class="w-tabs-section-content-h i-cf">
+	<div class="wpb_text_column ">
+		<div class="wpb_wrapper">
+			<p>The following are key aspects of my site that will help you navigate easly:-</p>
+			<p>The project has four key elements:</p>
+<ol>
+<li>Programs Panel
+	<ul>
+		<li> In the programs panel, one can be able to views diffrent key programs that have or are currently being undertaken by the UNEP.</li>
+		<li> Upload, Update or delete a Program - Once you click edit in a program. The information is displayed on the bottom form for one to edit.</li>
+	</ul>
+</li>
+<li>Map - View on the map the locations where Unep is currently conducting its programs</li>
+<li>Add
+	<ul>
+		<li> Add Regions into the database</li>
+		<li> Add Countries into the database</li>
+	</ul>
+</li>
+
+</ol>
+
+		</div>
+	</div>
+</div></div></div>
+
+</div></div></div></div></div></div></div></div></div></section><section class="l-section wpb_row height_medium"><div class="l-section-h i-cf"><div class="g-cols vc_row type_default valign_top"><div class="vc_col-sm-12 wpb_column vc_column_container"><div class="vc_column-inner"><div class="wpb_wrapper"></div></div></div></div></div></section>
+
+
+
+
+
+
+
+
+
+
+
+	
+		
+		</main>
+
+		
+	</div>
+</div>
+
+</div> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
